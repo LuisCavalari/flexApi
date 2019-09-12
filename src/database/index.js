40aadc8c93970ad9d29b6ec0641109dd3f8afc52
Luis Cavalari <lfelipe.felipe4@gmail.com>
2019-09-12 02:06:17 -0300

    clean code , i think

--- src/database/index.js
+++ src/database/index.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`,
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useFindAndModify:false,
+        useCreateIndex:true
     })
 
 module.exports = mongoose
--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const server = express();
-server.use(express.json());
 
-require('./app/controllers/authController')(server)
-require('./app/controllers/projectController')(server)
+server.use(express.json());
+require('./app/controllers')(server)
+
 
 server.get('/',(request,response)=>{
     response.send();
